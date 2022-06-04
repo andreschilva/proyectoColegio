3b490661325e5850a1cefb99d3b0f9ef3d428d21
Andres Silva <andreschilva3@gmail.com>
2022-06-03 23:53:04 -0400

    eliminando env.example

--- database/seeders/UsuariosTableSeeder.php
+++ database/seeders/UsuariosTableSeeder.php
@@ -29,6 +29,35 @@ class UsuariosTableSeeder extends Seeder
         $usuario->tipo ='sup';
         $usuario->save();
 
+        $usuario = new Usuario();
+        $usuario->id = 3;
+        $usuario->login = 'admin2';
+        $usuario->pass = md5('1234') ;
+        $usuario->perfil_id = 7;
+        $usuario->activo = 1;               
+        $usuario->eliminado = 0;
+        $usuario->tipo ='sup';
+        $usuario->save();
+
+        $usuario = new Usuario();
+        $usuario->id = 4;
+        $usuario->login = 'admin3';
+        $usuario->pass = md5('1234') ;
+        $usuario->perfil_id = 7;
+        $usuario->activo = 1;               
+        $usuario->eliminado = 0;
+        $usuario->tipo ='sup';
+        $usuario->save();
+
+        $usuario = new Usuario();
+        $usuario->id = 5;
+        $usuario->login = 'admin4';
+        $usuario->pass = md5('1234') ;
+        $usuario->perfil_id = 7;
+        $usuario->activo = 1;               
+        $usuario->eliminado = 0;
+        $usuario->tipo ='sup';
+        $usuario->save();
 
         
         
