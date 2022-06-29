fb95a334a7ace9ae57412c61fab3f54de90db4c3
Andres Silva <andreschilva3@gmail.com>
2022-06-29 06:59:24 -0400

    iteracion 3

--- app/Http/Controllers/HoraController.php
+++ app/Http/Controllers/HoraController.php
@@ -20,12 +20,25 @@ class HoraController extends Controller
         $buscar=$request->buscar;
         $pagina=$request->pagina;
         $resultado = $hora->obtenerHoras($buscar,$pagina);
+        foreach(session('permisos') as $modulo) {
+            foreach($modulo['funcionalidades'] as $funcionalidad ){
+                if( $funcionalidad['titulo'] == 'Horas'){ 
+                    $permisos = [
+                       'Permiso_mostrar'=> $funcionalidad['Permiso_mostrar'],
+                       'Permiso_modificar'=> $funcionalidad['Permiso_modificar'],
+                       'Permiso_Eliminar'=> $funcionalidad['Permiso_Eliminar'],
+                   ];
+                }
+                
+            }
+        }
         $mergeData = [
             'horas'=>$resultado['horas'],
             'total'=>$resultado['total'],
             'buscar'=>$buscar,
             'parPaginacion'=>$resultado['parPaginacion'],
-            'parControl'=>$this->parControl
+            'parControl'=>$this->parControl,
+            'permisos' => $permisos
         ];
         return view('horas.index',$mergeData);
     }
--- resources/views/horas/index.blade.php
+++ resources/views/horas/index.blade.php
@@ -55,9 +55,18 @@
                                     </td>
                                     <td>{{fecha_latina($hora->created_at) }}</td>
                                     <td data-texto="{{$hora->hora_ini}}">
+                                        @if ($permisos['Permiso_mostrar'] == 1) 
                                         <a href="{{route('horas.mostrar',$hora->id)}}" title="Mostrar"><img width="17px" src="{{asset('img/iconos/mostrar.png')}}" alt="Mostrar"></a>
+                                        @endif
+                                        @if ($permisos['Permiso_modificar'] == 1) 
                                         <a href="{{route('horas.modificar',$hora->id)}}" title="Modificar"><img width="17px" src="{{asset('img/iconos/modificar.png')}}" alt="Modificar"></a>
-                                        <a data-ruta="{{route('horas.eliminar',$hora->id)}}" class="btn-eliminar" title="Eliminar"><img width="17px" src="{{asset('img/iconos/eliminar.png')}}" alt="Eliminar"></a>
+                                        @endif
+                                        @if ($permisos['Permiso_Eliminar'] == 1) 
+                                        <a data-ruta="{{route('horas.eliminar',$hora->id)}}" class="btn-eliminar" title="Eliminar"><img width="17px" src="{{asset('img/iconos/eliminar.png')}}" alt="Eliminar"></a> 
+                                        @endif
+                                        
+                                        
+                                        
                                     </td>
                                 </tr>
                             @endforeach
