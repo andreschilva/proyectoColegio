0334083cb8e3576787b303d98efb2965fe7e4809
Andres Silva <andreschilva3@gmail.com>
2022-07-20 06:45:23 -0400

    pruebas

--- routes/webroutes/NotasRoutes.php
+++ routes/webroutes/NotasRoutes.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\NotaController;
 Route::get('notas', [NotaController::class,'index'])->name('notas.index')->middleware('sesion.iniciada');
 Route::get('notas/{id}', [NotaController::class,'grupos'])->name('notas.grupos')->middleware('sesion.iniciada');
 
-Route::get('notas/{id}/estudiantes', [NotaController::class,'estudiantes'])->name('notas.estudiantes')->middleware('sesion.iniciada');
+Route::get('notas\{id}\estudiantes', [NotaController::class,'estudiantes'])->name('notas.estudiantes')->middleware('sesion.iniciada');
 Route::get('notas/mostrar/{id}/{grupo}/{periodo?}', [NotaController::class,'mostrar'])->name('notas.mostrar')->middleware('sesion.iniciada');
 Route::get('notas/agregar/{id}/{grupo}/{periodo?}', [NotaController::class,'agregar'])->name('notas.agregar')->middleware('sesion.iniciada');
 Route::post('notas', [NotaController::class,'insertar'])->name('notas.insertar')->middleware('sesion.iniciada');
